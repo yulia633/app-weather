d2e482b48734b2a5b1a4fa049744d46d2e137b95
yulia633 <ignat633yulia@yandex.ru>
2021-05-31 11:09:53 +0300

    add base point api

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+require 'vendor/autoload.php'
+
+$app = new \Slim\App();
+
+$app->get('/locations/{id}', function ($request, $response, $args) {
+    return $response->withStatus(200)->write("Получено местоположение {$args['id']}.\
+    ");
+});
+
+$app->delete('/locations/{id}', function ($request, $response, $args) {
+    return $response->withStatus(200)->write("Удалено местоположение {$args['id']}.\
+    ");
+});
+
+$app->run();
