f106899eab75b0d711abf75c3662c1162b0ecb31
yulia633 <ignat633yulia@yandex.ru>
2021-05-31 11:15:08 +0300

    fix errors

--- index.php
+++ index.php
@@ -1,16 +1,16 @@
 <?php
 
-require 'vendor/autoload.php'
+require 'vendor/autoload.php';
 
 $app = new \Slim\App();
 
 $app->get('/locations/{id}', function ($request, $response, $args) {
-    return $response->withStatus(200)->write("Получено местоположение {$args['id']}.\
+    return $response->withStatus(200)->write("Получено местоположение {$args['id']}.
     ");
 });
 
 $app->delete('/locations/{id}', function ($request, $response, $args) {
-    return $response->withStatus(200)->write("Удалено местоположение {$args['id']}.\
+    return $response->withStatus(200)->write("Удалено местоположение {$args['id']}.
     ");
 });
 
