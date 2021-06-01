1dbc758260dd7d4888fa8399a13e80beb4ccb502
yulia633 <ignat633yulia@yandex.ru>
2021-06-01 21:55:50 +0300

    add db

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,5 @@
 FROM php:apache
 
 RUN docker-php-source extract && docker-php-ext-install mysqli && docker-php-source delete
+
+RUN apt-get update && apt-get install -y curl
--- index.php
+++ index.php
@@ -2,29 +2,65 @@
 
 require 'vendor/autoload.php';
 
-// Создать новый контейнер с внедренным Guzzle
+// Создать контейнер
 $container = new \Slim\Container([
-'http' => function () {
-    return new GuzzleHttp\Client();
-}
+    // Добавить в контейнер Guzzle под идентификатором http
+    'http' => function () {
+        return new GuzzleHttp\Client();
+    },
+    // Добавить в контейнер mysqli под идентификатором mysql
+    'mysql' => function () {
+        $mysqli = new mysqli(
+            getenv('DATABASE_HOST'),
+            getenv('DATABASE_USER'),
+            getenv('DATABASE_PASSWORD'),
+            getenv('DATABASE_NAME')
+        );
+        if ($mysqli->connect_errno) {
+            echo "Не удалось подключиться к MySQL {$mysqli->connect_error}";
+            exit;
+        } else {
+            return $mysqli;
+        }
+    },
 ]);
 
 // Создать объект App
 $app = new \Slim\App($container);
-
 // Получить погоду по идентификатору местоположения
 $app->get('/locations/{id}', function ($request, $response, $args) {
-    // Получить погоду из MetaWeather
-    $result = $this->http->get("https://www.metaweather.com/api/location/{$args['id']}")
-    ->getBody()
-    ->getContents();
-   // Вернуть результаты в виде JSON
+    // Получить местоположение из базы данных
+    $id = $this->mysql->real_escape_string($args['id']);
+    $results = $this->mysql->query("SELECT * FROM locations WHERE id='{$id}'");
+    // Если местоположение найдено, тогда получить прогноз погоды из БД, в противном случае сделать запрос к MetaWeather
+    if ($results->num_rows > 0) {
+        $result = $results->fetch_assoc()['weather'];
+    } else {
+        $result = $this->http->get("https://www.metaweather.com/api/location/{$id}")
+        ->getBody()
+        ->getContents();
+        $cleanResult = $this->mysql->real_escape_string($result);
+        if (
+            !$this->mysql->query("INSERT into locations (id, weather) VALUES ('{$id}', '{$cleanResult}')")
+        ) {
+            throw new Exception("Местоположение не может быть обновлено.");
+        }
+    }
+    // Возвратить результаты в формате JSON
     return $response->withStatus(200)->withJson(json_decode($result));
 });
 
 $app->delete('/locations/{id}', function ($request, $response, $args) {
-    return $response->withStatus(200)->write("Удалено местоположение {$args['id']}.
-    ");
+    // Получить местоположение из базы данных
+    $id = $this->mysql->real_escape_string($args['id']);
+    $results = $this->mysql->query("SELECT * FROM locations WHERE id='{$id}'");
+    // Если существует местоположение, удалить местоположение, иначе отправить ответ
+    // с кодом состояния 404
+    if ($results->num_rows > 0 && $this->mysql->query("DELETE FROM locations WHERE id='{$id}'")) {
+        return $response->withStatus(200)->write("Удалено местоположение {$args['id']}.");
+    } else {
+        return $response->withStatus(404)->write("Местоположение {$args['id']} не найдено.");
+    }
 });
 
 // Запуск приложения
